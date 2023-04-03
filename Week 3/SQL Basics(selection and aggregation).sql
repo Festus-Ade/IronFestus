aaecc7b4f1e999794f61d59b77a58713563d8043
Festus Oladipo <festusoladipo2015@gmail.com>
2023-04-03 22:30:20 +0200

    Update SQL Basics(selection and aggregation).sql

--- Week 3/SQL Basics(selection and aggregation).sql
+++ Week 3/SQL Basics(selection and aggregation).sql
@@ -85,3 +85,40 @@ SELECT type, COUNT(type) FROM bank.card
 GROUP BY type 
 ORDER BY COUNT(type) DESC;
 
+-- Using the loan table, print the top 10 account_ids based on the sum of all of 
+-- their loan amounts.
+SELECT sum(amount), account_id FROM bank.loan
+GROUP BY account_id
+ORDER BY sum(amount) desc
+LIMIT 10;
+
+-- In the loan table, retrieve the number of loans issued for each day, 
+-- before (excl) 930907, ordered by date in descending order.
+-- select * from bank.loan;
+SELECT date, count(amount) FROM bank.loan
+WHERE date < 930907 
+GROUP BY date
+ORDER BY date DESC;
+
+-- In the loan table, for each day in December 1997, count the number of loans issued 
+-- for each unique loan duration, ordered by date and duration, both in ascending order. 
+-- You can ignore days without any loans in your output.
+-- SELECT date, duration, COUNT(loan_id)
+-- I'm not sure about this question
+
+-- In the trans table, for account_id 396, sum the amount of transactions for each type 
+-- (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type 
+-- and the sum of amount, named as total_amount. Sort alphabetically by type.
+select account_id, type, sum(amount) AS total_amount from bank.trans
+WHERE account_id = 396
+GROUP BY type ORDER BY type;
+
+-- From the previous output, translate the values for type to English, rename the column
+-- to transaction_type, round total_amount down to an integer
+
+-- From the previous result, modify your query so that it returns only one row, with a column
+-- for incoming amount, outgoing amount and the difference.
+
+-- Continuing with the previous example, rank the top 10 account_ids based on their
+--  difference.
+
